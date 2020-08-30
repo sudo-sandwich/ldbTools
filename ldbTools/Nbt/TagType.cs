76264468f861f13b5bcd532aeddc77e570e9223a
Misys <luskconrad@gmail.com>
2020-08-30 18:26:05 -0400

    no functional changes, just made TagType a bit prettier

--- ldbTools/Nbt/TagType.cs
+++ ldbTools/Nbt/TagType.cs
@@ -9,18 +9,18 @@ namespace ldbTools.Nbt {
     /// An enum backed by a byte value that can be translated to NBT tag IDs.
     /// </summary>
     public enum TagType : byte {
-        End = 0,
-        Byte = 1,
-        Short = 2,
-        Int = 3,
-        Long = 4,
-        Float = 5,
-        Double = 6,
-        ByteArray = 7,
-        String = 8,
-        List = 9,
-        Compound = 10,
-        IntArray = 11,
-        LongArray = 12
+        End         = 0x00,
+        Byte        = 0x01,
+        Short       = 0x02,
+        Int         = 0x03,
+        Long        = 0x04,
+        Float       = 0x05,
+        Double      = 0x06,
+        ByteArray   = 0x07,
+        String      = 0x08,
+        List        = 0x09,
+        Compound    = 0x0A,
+        IntArray    = 0x0B,
+        LongArray   = 0x0C
     }
 }
